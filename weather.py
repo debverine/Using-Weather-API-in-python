1a376b163075b24680c22bd2e78757d09923fb36
Debargha Bose <debverine@users.noreply.github.com>
2018-03-08 16:53:58 +0530

    Update weather.py
    tried my hand at showing only the essential weather data for some cities

--- weather.py
+++ weather.py
@@ -1,8 +1,23 @@
 import requests
 
-api_address='http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
-city = input('City Name :')
-url = api_address + city
-json_data = requests.get(url).json()
-format_add = json_data['base']
-print(format_add)
+
+def weather(city):
+    api_address='http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
+    #city = input('City Name :')
+    url = api_address + city
+    json_data = requests.get(url).json()
+    #print(json_data)
+    
+    weather_description = json_data['weather'][0]['description']
+    print("City : "+city)
+    print(weather_description)
+    temp=json_data['main']['temp']-273.15
+    humidity=json_data['main']['humidity']
+    print(str(temp)+" C")
+    print(str(humidity)+" %")
+    
+
+list=["Kolkata","Mumbai","Delhi","Bangalore","Dubai","Ahmedabad"]
+for i in list:
+    weather(i)
+    print("************************")
